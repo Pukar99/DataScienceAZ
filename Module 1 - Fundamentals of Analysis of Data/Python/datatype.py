c529bd5265cacb227ad1b02c09f377d9b33becf5
Pukar Sharma <sharma.pukar99@gmail.com>
2024-04-17 21:33:48 +0545

    task 1

---
+++ Module 1 - Fundamentals of Analysis of Data/Python/datatype.py
@@ -0,0 +1,77 @@
+# Define integers
+num1 = 10
+num2 = 5
+
+# Define floats
+float1 = 10.5
+float2 = 5.5
+
+# Define strings
+string1 = "Hello"
+string2 = "World"
+
+# Define booleans
+bool1 = True
+bool2 = False
+
+# Define lists
+list1 = [1, 2, 3, 4, 5]
+list2 = ["apple", "banana", "cherry"]
+
+# Define tuples
+tuple1 = (1, 2, 3, 4, 5)
+tuple2 = ("apple", "banana", "cherry")
+
+# Define sets
+set1 = {1, 2, 3, 4, 5}
+set2 = {"apple", "banana", "cherry"}
+
+# Define dictionaries
+dict1 = {"name": "John", "age": 36}
+dict2 = {"apple": 2, "banana": 3, "cherry": 5}
+
+# Print the data types
+print(type(num1))
+print(type(float1))
+print(type(string1))
+print(type(bool1))
+print(type(list1))
+print(type(tuple1))
+print(type(set1))
+print(type(dict1))
+
+# Perform basic arithemetic operations
+# Addition
+print(num1 + num2)
+
+# Subtraction
+print(num1 - num2)
+
+# Multiplication
+print(num1 * num2)
+
+# Division
+print(num1 / num2)
+
+# Modulus
+print(num1 % num2)
+
+# Exponentiation
+
+print(num1 ** num2)
+
+# Floor division
+print(num1 // num2)
+
+# Comparison operators
+# Equal to
+print(num1 == num2)
+
+# Not equal to
+print(num1 != num2)
+
+# Greater than
+print(num1 > num2)
+
+# Less than
+print(num1 < num2)
