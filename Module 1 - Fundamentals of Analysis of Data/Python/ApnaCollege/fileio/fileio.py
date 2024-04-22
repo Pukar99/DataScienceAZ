2636fc1791b1f497a2dd38faf049f2d71148b00d
Pukar Sharma <sharma.pukar99@gmail.com>
2024-04-22 10:57:37 +0545

    file IO

--- Module 1 - Fundamentals of Analysis of Data/Python/ApnaCollege/fileio/fileio.py
+++ Module 1 - Fundamentals of Analysis of Data/Python/ApnaCollege/fileio/fileio.py
@@ -1,26 +1,52 @@
-#File I/O in Python
-#Python can be used to perform operations on a file.
-#File operations are read, write, append, and delete.
-#Types of all files:
-#1. Text files (txt, csv, etc.)
-#2. Binary files (mp4, mp3, pdf, etc.)
-#3. JSON files
+# # File I/O in Python
+# # Python can be used to perform operations on a file.
+# # File operations are read, write, append, and delete.
+# # Types of all files:
+# # 1. Text files (txt, csv, etc.)
+# # 2. Binary files (mp4, mp3, pdf, etc.)
+# # 3. JSON files
 
-# f = open("file_name", "mode")
-# modes: r, w, a, r+, w+, a+
-# r: read
-# w: write
-# a: append
-# r+: read and write
-# w+: write and read
-# a+: append and read
+# # f = open("file_name", "mode")
+# # modes: r, w, a, r+, w+, a+
+# # r: read
+# # w: write
+# # a: append
+# # r+: read and write
+# # w+: write and read
+# # a+: append and read
 
-# data = f.read()
-#f.close()
+# # data = f.read()
+# # f.close()
 
-f = open("demo.txt", "r")
-data = f.read()
-print(data)
+# # f = open("demo.txt", "r")
+# # data = f.read()
+# # print(data)
+# # print(type(data))
+# # f.close()
+
+# # Output: Pukar
+
+# f = open("demo.txt", "r")
+# data = f.read(2)
+# print(data)
+# f.close()
+
+
+# f = open("demo.txt", "r")
+# line1 = f.readline()
+# line2 = f.readline()
+# print(line1)
+# print(line2)
+# f.close()
+
+# # Read () and Readline() are used to read the file.
+# # Read() reads the whole file.
+# # Readline() reads the first line of the file.
+
+# f = open("demo.txt", "a")
+# f.write("I want to Learn javascript \n")
+# f.close()
+
+f = open("sample1.txt", "a")
+f.write("I want to Learn javascript \n")
 f.close()
-
-#Output: Hello, this is a demo file.
